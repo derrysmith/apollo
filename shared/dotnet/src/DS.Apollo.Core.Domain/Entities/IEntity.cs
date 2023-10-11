9e900d0dfa7a3d9d8399d863b6926fdb82ebd6d9
Derry Smith <derrysmith@gmail.com>
2023-10-10 17:18:40 -0700

    add audit timestamps to entity

--- shared/dotnet/src/DS.Apollo.Core.Domain/Entities/IEntity.cs
+++ shared/dotnet/src/DS.Apollo.Core.Domain/Entities/IEntity.cs
@@ -1,6 +1,12 @@
 ﻿namespace DS.Apollo.Core.Domain.Entities;
 
-public interface IEntity<out TEntityKey>
+public interface IEntity
+{
+	DateTimeOffset CreatedAt { get; }
+	DateTimeOffset UpdatedAt { get; }
+}
+
+public interface IEntity<out TEntityKey> : IEntity
 	where TEntityKey : IEntityKey
 {
 	TEntityKey Id { get; }
