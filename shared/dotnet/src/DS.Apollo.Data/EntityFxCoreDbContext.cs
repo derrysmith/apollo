293a48049b746238e5d326acf14b7abef9eedc6c
Derry Smith <derrysmith@gmail.com>
2023-10-10 21:31:43 -0700

    add base dbcontext implementation

---
+++ shared/dotnet/src/DS.Apollo.Data/EntityFxCoreDbContext.cs
@@ -0,0 +1,37 @@
+﻿using DS.Apollo.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DS.Apollo.Data;
+
+public abstract class EntityFxCoreDbContext : DbContext
+{
+	protected EntityFxCoreDbContext(DbContextOptions options, string? schema = default)
+		: base(options)
+	{
+		this.DefaultSchema = schema ?? "dbo";
+	}
+
+	protected string DefaultSchema { get; }
+
+	public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+	{
+		// add audit timestamps
+		var currentDateTime = DateTimeOffset.UtcNow;
+
+		foreach (var entry in this.ChangeTracker.Entries<IEntity>())
+		{
+			switch (entry)
+			{
+				case { State: EntityState.Added }:
+					entry.Property(e => e.CreatedAt).CurrentValue = currentDateTime;
+					entry.Property(e => e.UpdatedAt).CurrentValue = currentDateTime;
+					break;
+				case { State: EntityState.Modified }:
+					entry.Property(e => e.UpdatedAt).CurrentValue = currentDateTime;
+					break;
+			}
+		}
+
+		return base.SaveChangesAsync(cancellationToken);
+	}
+}
