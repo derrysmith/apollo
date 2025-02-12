54a6b21aed8c8db427fdf9e6071b6e3735495272
derrysmith <derrysmith@gmail.com>
2025-02-12 15:33:34 -0800

    build and test

--- apollo-sdk/test/Apollo.Tests/UnitTest1.cs
+++ apollo-sdk/test/Apollo.Tests/UnitTest1.cs
@@ -1,35 +1,29 @@
-// using System.Reflection;
-// using Apollo.Entities;
-//
-// namespace Apollo.Tests.Entities;
-//
-// public class AggregateRootTests;
-//
-// public class EntityTests;
-//
-// public class EntityKeyAttributeTests;
-//
-// public class EntityKeyTests
-// {
-// 	// UserId.New();
-// 	// var userId = new UserId();
-// 	// var userId = UserId.Parse("usr_123ABC");
-// 	
-// 	// [EntityKey(Format = "usr_{0}", GeneratorType = EntityKeyGeneratorType.Ulid)]
-// 	// record UserId : EntityKey<UserId>;
-// 	
-// 	[Fact]
-// 	public void ctor_returnsNewInstance()
-// 	{
-// 		// arrange
-// 		
-// 		// act
-// 		var actual = new UserProfileId();
-// 		
-// 		// assert
-// 		Assert.StartsWith("usr_", actual.ToString());
-// 	}
-// }
-//
-// [EntityKey(Format = "usr_{0}", GeneratorType = EntityKeyGeneratorType.SequentialDate)]
-// public record UserProfileId : EntityKey<UserProfileId>;
+using System.Reflection;
+using Apollo.Entities;
+
+namespace Apollo.Tests.Entities;
+
+public class EntityKeyTests
+{
+	// UserId.New();
+	// var userId = new UserId();
+	// var userId = UserId.Parse("usr_123ABC");
+	
+	// [EntityKey(Format = "usr_{0}", GeneratorType = EntityKeyGeneratorType.Ulid)]
+	// record UserId : EntityKey<UserId>;
+	
+	[Fact]
+	public void ctor_returnsNewInstance()
+	{
+		// arrange
+		
+		// act
+		var actual = new UserProfileId();
+		
+		// assert
+		Assert.StartsWith("usr_", actual.ToString());
+	}
+}
+
+[EntityKey(Format = "usr_{0}", GeneratorType = EntityKeyGeneratorType.SequentialDate)]
+public record UserProfileId : EntityKey<UserProfileId>;
