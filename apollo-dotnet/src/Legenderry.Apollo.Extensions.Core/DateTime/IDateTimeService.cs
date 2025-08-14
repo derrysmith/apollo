5698bcd2080a39e1e888020709a1cfdca9b750a0
derrysmith <derrysmith@gmail.com>
2025-08-13 20:10:37 -0700

    add datetime service

---
+++ apollo-dotnet/src/Legenderry.Apollo.Extensions.Core/DateTime/IDateTimeService.cs
@@ -0,0 +1,13 @@
+namespace Legenderry.Apollo.Extensions.Core.DateTime;
+
+public interface IDateTimeService
+{
+	DateTimeOffset Now    { get; }
+	DateTimeOffset UtcNow { get; }
+}
+
+public class DateTimeService : IDateTimeService
+{
+	public DateTimeOffset Now    => DateTimeOffset.Now;
+	public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
+}
